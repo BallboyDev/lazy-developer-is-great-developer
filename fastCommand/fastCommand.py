3c334294eff63558bf731e4e1d2104438688ff70
yswgood0329 <yswgood0329@gmail.com>
2021-01-07 14:21:29 +0900

    fastCommand 기본 기능 구현

--- fastCommand/fastCommand.py
+++ fastCommand/fastCommand.py
@@ -1,24 +1,46 @@
 import json, os, sys
 
-class jsonData:
+class JsonData:
     def __init__(self):
         with open('./command.json', 'r') as jsonData:
-            data = json.load(jsonData)
-        self.setPath(data["programPath"])
-        self.setCommand(data["command"])
+            self.__command = json.load(jsonData)
 
-        
-    def setPath(self, path):
-        self.path = path
-    
-    def setCommand(self, command):
-        self.command = command
+    @property
+    def path(self):
+        return self.__path
+
+    @path.setter
+    def path(self, path):
+        self.__path = path
+
+    @property
+    def command(self):
+        return self.__command
+
+    @command.setter
+    def command(self, command):
+        self.__command = command
+
+    def getData(self, keys, tree = {}):
+        command = self.__command if len(tree) == 0 else tree
+
+        if str(type(command[keys[0]])) == "<class 'dict'>":
+            self.getData(['init'] if len(keys[1:]) == 0 else keys[1:], command[keys[0]])
+
+        # elif str(type(command[keys[0]])) == "<class 'list'>":
+        #     self.execute(command[keys[0]])
+
+        elif str(type(command[keys[0]])) == "<class 'str'>":
+            self.execute(command[keys[0]])
+
+    def execute(self, command):
+        os.system(f'{command}')
 
     def toString(self):
         print("=== path ===")
-        print(json.dumps(self.path, indent='\t'))
+        print(json.dumps(self.__path, indent='\t'))
         print('=== command ===')
-        print(json.dumps(self.command, indent='\t'))
+        print(json.dumps(self.__command, indent='\t'))
 
 if __name__ == '__main__':
-    jsonData().toString()
+    JsonData().getData(sys.argv[1:])
