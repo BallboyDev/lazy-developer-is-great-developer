62ec0dcb095f6544ae324a2dd0b4b4898dbb0271
yswgood0329 <yswgood0329@gmail.com>
2021-01-20 17:27:55 +0900

    90% 완성 - 귀차니즘의 끝판왕 개발

--- fastCommand/batch/fastCommand.bat
+++ fastCommand/batch/fastCommand.bat
@@ -1,30 +1,65 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
-echo %*
 
 for %%x in (%*) do (
-	set /a "i+=1"
-	call set path!i!=%%~!i!
+	set /a "paramLen+=1"
+	call set path!paramLen!=%%~!paramLen!
 )
 
 set index=1
+call set param=%%path!index!%%
 
-for /f "tokens=1,2 delims=:" %%a in (test.json) do (
-	call set param=%%path!index!%%
-	set cmd=%%a
-	REM 한칸 띄어쓰기 제거
-	set cmd=!cmd: =!
-	REM Tab 공백 제거
-	set cmd=!cmd:	=!
-	rem 쌍따옴표 제거
-	set cmd=!cmd:"=!
+for /f "tokens=1,2 delims=:" %%a in (command.json) do (
 	
-	if !cmd!==!param! (
-		echo !cmd! / !param!
-		set /a "index+=1"
-		call set param=%%path!index!%%
-		echo %%b
+	set json=%%a
+	REM 한칸 띄어쓰기 제거
+	set json=!json: =!
+	REM Tab 공백 제거
+	set json=!json:	=!
+	rem 쌍따옴표 제거
+	set json=!json:"=!
+	
+	if !json!==!param! (
+
+
+		if !paramLen!==!index! (
+			set cmd=%%b
+			set cmd=!cmd: =!
+			set cmd=!cmd:	=!
+			@REM set cmd=!cmd:{=!
+
+			if !cmd!=={ (
+
+				call set param=init
+			)
+
+			if not !cmd!=={ (
+				set exe=%%b
+				set exe=!exe:"=!
+				set exe=!exe:,=!
+
+				echo !exe!
+				goto execute
+			)
+		)
+
+		if not !paramLen!==!index! (
+
+			set /a "index+=1"
+			call set param=%%path!index!%%
+		)
+		
 	)
 )
+goto error
 
+:execute
+start !exe!
+goto finish
+
+:error
+echo not found cmd
+
+:finish
+exit
 ENDLOCAL
