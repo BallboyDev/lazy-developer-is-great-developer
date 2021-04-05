a2e5a68eb0c40efaef9fe26c8b95e16489297d80
ballboyDev <yswgood0329@gmail.com>
2021-04-05 11:33:10 +0900

    쿼리 변환 수정

--- 코드 반복 작성/web version/index.js
+++ 코드 반복 작성/web version/index.js
@@ -20,21 +20,36 @@ window.onload = () => {
         switch (this.radioStatus()) {
             case 'query':
                 let index = 0
-                form = this.document.getElementById('form').value
-                data = this.document.getElementById('data').value.split(',').map((v1, i) => {
+                form = this.document.getElementById('form').value.replace(/[\[](.*?)[\]][\[](.*?)[\]]==>(.*?): /gi, '')
+                data = this.document.getElementById('data').value.replace(/[\[](.*?)[\]][\[](.*?)[\]]==>(.*?): /gi, '').split(',').map((v1, i) => {
 
-                    //.trim().split(/[(](.*?)[)]/gi)
-                    if (v1.indexOf('(') > -1 && v1.indexOf(')')) {
+                    if ((v1.indexOf('(') > -1 && v1.indexOf(')')) || (v1.indexOf('{') > -1 && v1.indexOf('}'))) {
+                        let colName = ''; let colType = ''; let value = '';
+                        value = v1
+                        if (v1.indexOf('(') > -1 && v1.indexOf(')')) {
+                            let type = v1.match(/[(](.*?)[)]/gi)
+                            colType = type[type.length - 1]
+                            value = value.replace(colType, '')
+                        }
+                        if (v1.indexOf('{') > -1 && v1.indexOf('}')) {
+                            let name = v1.match(/[{](.*?)[}]/gi)
+                            colName = name[name.length - 1]
+                            value = value.replace(colName, '')
+                        }
+
                         return {
-                            colName: v1.trim().match(/[(](.*?)[)]/gi)[0].replace(/[(]|[)]/gi, ''),
-                            value: v1.trim().replace(/[(](.*?)[)]/gi, '')
+                            colName: colName.replace(/[{]|[}]/gi, '') || `colName${i}`,
+                            colType: colType.replace(/[(]|[)]/gi, '') || `colType${i}`,
+                            value: value.trim()
                         }
                     } else {
                         return {
                             colName: `colName${i}`,
+                            colType: `colType${i}`,
                             value: v1.trim()
                         }
                     }
+
                 }).filter((v) => { return (v || '') !== '' })
 
                 form = form.split('').map((v) => {
@@ -46,8 +61,13 @@ window.onload = () => {
                 }).join('')
 
                 data.map((v) => {
-                    let reg1 = new RegExp(`#{${v.colName}}`, 'gi')
-                    form = form.replace(reg1, `'${v.value}'`)
+                    let reg1 = new RegExp(`#{${v.colName}}`, 'gi') // 
+                    if (['NUMBER', 'DECIMAL', 'INT', 'INTEGER', 'FLOAT', 'BIT', 'TIUNYINT', 'SMALLINT', 'NUMERIC'].findIndex((t) => { return v.colType.toUpperCase() === t }) > -1) {
+                        form = form.replace(reg1, `${v.value}`)
+                    } else {
+                        form = form.replace(reg1, `'${v.value}'`)
+                    }
+
                 })
 
                 this.document.getElementById('result').value = form
@@ -79,7 +99,7 @@ window.onload = () => {
                 console.error('에러 발생...???!!!! 왜지?!?!!!???!!')
                 console.error('이거 출력되면... 승우한테 문의 하.... 지 말아주세요')
                 console.error('저도 이게 어떻게 돌아가는지 지금은 잘.... 기억이 안날꺼 같아요.......')
-                console.error('이게 발생 할 수가... 있나?!!?!??!?!??!?!')
+                console.error('인간적으로 이게 발생 할 수가... 있나?!!?!??!?!??!?!')
                 break;
         }
     }
@@ -89,9 +109,9 @@ window.onload = () => {
     this.document.getElementById('menual').onclick = () => {
         switch (this.radioStatus()) {
             case 'query':
-                this.document.getElementById('form').value = '----------예시1---------- \nSELECT * \n  FROM TEMP_TABLE T \n WHERE CO_CD = ? \n   AND DIV_CD = ? \n   AND EMP_CD = ?\n\n----------예시2---------- \nSELECT * \n  FROM TEMP_TABLE T \n WHERE CO_CD = #{coCd} \n   AND EMP_CD = #{empCd} \n   AND DIV_CD = #{divCd}'
-                this.document.getElementById('data').value = '1000(coCd), 2000(divCd), 3000(empCd)'
-                this.document.getElementById('result').value = '< query 변환 사용법 >\n1. form 입력칸(좌상단)에 쿼리를 입력한다.\n2. data 입력칸(좌하단)에 데이터를 입력한다.\n3. change 버튼을 클릭한다.\n\nform칸에 입력된 쿼리에서 \'?\' 문자는 쿼리 변환시 data칸에 입력된 데이터를 입력 라인 순서대로 변환됩니다.\n\nform칸에 Mybatis와 같이 #{colName} 형식으로 입력시에는 data칸에 입력된 데이터의 colName에 맞춰 변환이 이루어 집니다.\n\n----------예시1---------- \nSELECT * \n  FROM TEMP_TABLE T \n WHERE CO_CD = \'1000\' \n   AND DIV_CD = \'2000\' \n   AND EMP_CD = \'3000\'\n\n----------예시2---------- \nSELECT * \n  FROM TEMP_TABLE T \n WHERE CO_CD = \'1000\' \n   AND DIV_CD = \'2000\' \n   AND EMP_CD = \'3000\''
+                this.document.getElementById('form').value = `[DEBUG][2021-04-02 16:57:20]==>  Preparing: /* 회계기수 정보 조회 단건*/ SELECT MAX(G.GISU) AS GISU , MAX(G.FR_DT) AS FR_DT , MAX(G.TO_DT) AS TO_DT , G.RMK_DC , E.KOR_NM FROM erp_1234.SGISU G LEFT OUTER JOIN erp_1234.SEMP E ON E.CO_CD = G.CO_CD AND E.EMP_CD = G.INSERT_ID WHERE G.CO_CD = ? AND ? BETWEEN G.FR_DT AND G.TO_DT `
+                this.document.getElementById('data').value = `[DEBUG][2021-04-02 16:57:20]==> Parameters: 2000(String), 20210402(String)`
+                this.document.getElementById('result').value = `< query 변환 사용법 >\n1. form 입력칸(좌상단)에 쿼리를 입력한다.\n2. data 입력칸(좌하단)에 데이터를 입력한다.\n3. change 버튼을 클릭한다.\n\nform칸에 입력된 쿼리에서 '?' 문자는 쿼리 변환시 data칸에 입력된 데이터를 입력 라인 순서대로 변환됩니다.\n\nform칸에 Mybatis와 같이 #{colName} 형식으로 입력시에는 data칸에 입력된 데이터의 colName에 맞춰 변환이 이루어 집니다.\n\n----------form - 예시1---------- \nSELECT * \n  FROM TEMP_TABLE T \n WHERE CO_CD = ?\n   AND EMP_CD = ?\n   AND DIV_CD = ?\n\n----------data - 예시1---------- \n1000(String), 2000(String), 3000(String)\n\n\n\n----------form - 예시2---------- \nSELECT * \n  FROM TEMP_TABLE T \n WHERE CO_CD = #{coCd}\n   AND EMP_CD = #{divCd}\n   AND DIV_CD = #{empCd}\n\n----------data - 예시2---------- \n1000(String){coCd}, 2000(String){divCd}, 3000(String){empCd}\n\n---------result--------- \nSELECT * \n  FROM TEMP_TABLE T \n WHERE CO_CD = '1000'\n   AND EMP_CD = '2000'\n   AND DIV_CD = '3000'`
                 break;
             case 'repeat':
                 this.document.getElementById('data').value = '1-1\t1-2\t1-3\t1-4\n2-1\t2-2\t2-3\t2-4\n3-1 3-2 3-3 3-4\n4-1 4-2 4-3 4-4\n5-1 5-2 5-3 5-4\n6-1 6-2 6-3 6-4';
